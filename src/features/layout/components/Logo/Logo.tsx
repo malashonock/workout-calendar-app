d0f19f5337fbae62721fcc46fa3c1b8014be5b56
Yan Malashonak <yan.malashonak@gmail.com>
2023-05-08 23:29:58 +0300

    feat(layout): conver Logo into a link to the home page

--- src/features/layout/components/Logo/Logo.tsx
+++ src/features/layout/components/Logo/Logo.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 import { Stack, Typography } from '@mui/material';
 
 import styles from './Logo.module.scss';
@@ -6,13 +7,15 @@ import logoImagePath from 'assets/images/logo.png';
 
 export const Logo: FunctionComponent = () => {
   return (
-    <Stack direction="row" gap={1}>
-      <img src={logoImagePath} alt="logo" height="40" width="40" />
-      <Typography className={styles.text}>
-        Workout
-        <br />
-        Calendar
-      </Typography>
-    </Stack>
+    <Link to="/" className={styles.link}>
+      <Stack direction="row" gap={1}>
+        <img src={logoImagePath} alt="logo" height="40" width="40" />
+        <Typography className={styles.text}>
+          Workout
+          <br />
+          Calendar
+        </Typography>
+      </Stack>
+    </Link>
   );
 };
